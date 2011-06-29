bb6bd8c3985f136aa3d236fb59750a7dfefaa6eb
Jeziel Jones <impulse.ly@gmail.com>
2011-06-29 12:12:27 -0700

    Improved the README file

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Impulse App
+
+This is my first app...let's hope it turns out well. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Impulse::Application.config.secret_token = 'b4b47a0331985662c1503650241c8cbbef0f8fb64cef59b0470ff155e9d83ad06b5976c16fc5ee924a82e34ba1f04424503b97f3beae3ef26e1e3170cc2a1b43'
